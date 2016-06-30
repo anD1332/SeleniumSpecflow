1e6be4345371ec68fdc524622810e5c13dab8218
Andy Harding <andyharding04@gmail.com>
2016-06-30 20:38:21 +0100

    Initial solution commit

---
+++ src/SeleniumSpecflowMasterclass/DriverStore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.Remoting.Channels;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumSpecflowMasterclass
+{
+    public class DriverStore
+    {
+        [ThreadStatic] public static IWebDriver Driver;
+
+        public static IWebDriver GetForCurrentThread()
+        {
+            if (Driver != null)
+            {
+                return Driver;
+            }           
+
+            Trace.WriteLine($"Creating driver context for thread {Thread.CurrentThread.Name}");
+
+            Driver = new ChromeDriver();
+
+            var driver = Driver;
+
+            AppDomain.CurrentDomain.DomainUnload += (IChannelSender, args) => driver.Quit();
+
+            return Driver;
+        }
+
+
+    }
+}
