51fb9941fa9f976e9d7207d8e816d78c848803a9
Andy Harding <andyharding04@gmail.com>
2016-07-01 08:30:53 +0100

    Removed initial space from headphones title

--- src/SeleniumSpecflowMasterclass/Constants.cs
+++ src/SeleniumSpecflowMasterclass/Constants.cs
@@ -4,6 +4,6 @@
     {
         public const string HeadphonesCategoryPageUrl = @"http://test.dutyfree.maginfrastructure.com/store/dixons-travel/headphones/c-24/c-116";
 
-        public const string HeadphonesCategoryPageTitle = " Headphones | Dixons Travel | Official website for Manchester Duty Free Shopping | Manchester";
+        public const string HeadphonesCategoryPageTitle = "Headphones | Dixons Travel | Official website for Manchester Duty Free Shopping | Manchester";
     }
 }
