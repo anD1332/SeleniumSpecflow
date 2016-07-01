4d5dbc671c273733fb57ccabc07c29a06da37830
Andy Harding <andyharding04@gmail.com>
2016-07-01 08:30:06 +0100

    Added constants file with url and page title

---
+++ src/SeleniumSpecflowMasterclass/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace SeleniumSpecflowMasterclass
+{
+    public class Constants
+    {
+        public const string HeadphonesCategoryPageUrl = @"http://test.dutyfree.maginfrastructure.com/store/dixons-travel/headphones/c-24/c-116";
+
+        public const string HeadphonesCategoryPageTitle = " Headphones | Dixons Travel | Official website for Manchester Duty Free Shopping | Manchester";
+    }
+}
