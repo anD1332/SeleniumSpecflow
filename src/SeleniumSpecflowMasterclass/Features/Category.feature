0c666348069ff1661b3b654024468808c61ede65
Andy Harding <andyharding04@gmail.com>
2016-07-14 22:07:47 +0100

    Commit

---
+++ src/SeleniumSpecflowMasterclass/Features/Category.feature
@@ -0,0 +1,9 @@
+﻿@Category
+Feature: Category
+    As a ecommerce provider 
+    I want to provide a category page
+    so that customers can view all products for a particular category
+
+Scenario: The category page h1 contains the category name
+    Given I am on the Headphones Category Page
+    Then the header is Headphones
---
+++ src/SeleniumSpecflowMasterclass/Steps/Category.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace SeleniumSpecflowMasterclass.Steps
+{
+    [Binding, Scope(Feature = "Category")]
+    public class CategorySteps
+    {
+        [Given(@"I am on the Headphones Category Page")]
+        public void GivenIAmOnThe()
+        {
+            var category = "Headphones";
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(Constants.HeadphonesCategoryPageUrl);
+            driver.Title.Should().Be($"{category} | Dixons Travel | Official website for Manchester Airport Duty Free Shopping | Manchester Airport");
+        }
+
+        [Then(@"the header is Headphones")]
+        public void ThenTheHeaderIs()
+        {
+            IWebDriver driver = new ChromeDriver();
+            IWebElement header = driver.FindElement(By.TagName("h1"));
+            var actualHeader = header.Text.Replace("\r\n", " ");
+            actualHeader.Should().Be("Headphones");
+        }
+    }
+}
